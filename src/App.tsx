a3a59a5b8ed73eb103cb1aa55ffad96340940ad5
Marika Pescosolido <marikapescosolido@gmail.com>
2025-09-27 06:08:20 -0400

    ch7

--- src/App.tsx
+++ src/App.tsx
@@ -5,12 +5,7 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Index from "./pages/Index";
 import GetLegalHelp from "./pages/GetLegalHelp";
-<<<<<<< HEAD
-import GetLegalHelpWrapper from "./pages/GetLegalHelpWrapper";
-import SubmitCase from "./pages/SubmitCase";
 import PersonalDetails from "./pages/PersonalDetails";
-=======
->>>>>>> cc23cf66af5e7ec4fc7c9b235ab050ceadf32671
 import NotFound from "./pages/NotFound";
 import Professionals from "./pages/Professionals";
 
@@ -26,12 +21,7 @@ const App = () => (
           <Route path="/" element={<Index />} />
           <Route path="/get-legal-help" element={<GetLegalHelp />} />
           <Route path="/professionals" element={<Professionals />} />
-<<<<<<< HEAD
-          <Route path="/get-legal-help/apply" element={<ApplyForm />} />
           <Route path="/get-legal-help/personal-details" element={<PersonalDetails />} />
-          <Route path="/submit-case" element={<SubmitCase />} />
-=======
->>>>>>> cc23cf66af5e7ec4fc7c9b235ab050ceadf32671
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
