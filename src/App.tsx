f3611e2042be23cad8584d5f2812f65d8143e82e
Marika Pescosolido <marikapescosolido@gmail.com>
2025-09-27 05:20:58 -0400

    ch3

--- src/App.tsx
+++ src/App.tsx
@@ -19,6 +19,7 @@ const App = () => (
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/get-legal-help" element={<GetLegalHelp />} />
+          <Route path="/professionals" element={<Professionals />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
